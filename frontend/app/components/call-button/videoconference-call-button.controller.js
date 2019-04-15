2f092290575284bd1a47b689974d8ab14b119721
Christophe HENRY <chenry@linagora.com>
2019-04-15 16:57:47 +0200

    Solves esn#2634: Popover should display disabled action buttons on external users and self

--- frontend/app/components/call-button/videoconference-call-button.controller.js
+++ frontend/app/components/call-button/videoconference-call-button.controller.js
@@ -4,12 +4,14 @@
   angular.module('linagora.esn.videoconference')
     .controller('videoconferenceCallButtonController', videoconferenceCallButtonController);
 
-  function videoconferenceCallButtonController(VideoConfCallService) {
+  function videoconferenceCallButtonController(VideoConfCallService, session) {
     var self = this;
 
     self.launch = launch;
+    self.isCurrentUser = self.userId === session.user._id;
 
     function launch(userId) {
+      if (self.isCurrentUser) return;
       VideoConfCallService.call(userId);
     }
   }
